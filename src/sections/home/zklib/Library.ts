5bff638a1d7fc623022f5331bc499b52ee54c12b
Jensen <jensenczx@gmail.com>
2023-04-28 17:12:40 +0800

    fix zk lib error

---
+++ src/sections/home/zklib/Library.ts
@@ -0,0 +1,145 @@
+import { MerkleTree } from './MerkleTree';
+import { poseidon1 } from './Poseidon';
+
+const snarkjs = require('snarkjs');
+
+// export async function generateSigProofCallData(
+//   privateKey: string,
+//   circuitWasmBuffer: Buffer,
+//   zkeyBuffer: Buffer
+// ): Promise<string> {
+//   const privateKeyChunks = splitPrivateKey(privateKey);
+//   const inputs = await generateSigCircuitInputJson(privateKeyChunks);
+//   console.log('Debug::start prove');
+//   const { proof, publicSignals } = await snarkjs.plonk.fullProve(
+//     inputs,
+//     circuitWasmBuffer,
+//     zkeyBuffer
+//   );
+//   console.log('Debug::end prove');
+//   const proofProcessed = unstringifyBigInts(proof);
+//   const pubProcessed = unstringifyBigInts(publicSignals);
+//   const allSolCallData: string = await snarkjs.plonk.exportSolidityCallData(
+//     proofProcessed,
+//     pubProcessed
+//   );
+//   const solCallDataProof = allSolCallData.split(',')[0];
+//   return solCallDataProof;
+// }
+
+// export async function generateMerkleProofCallData(
+//   merkleTree: MerkleTree,
+//   mainAddr: BigInt,
+//   receiverAddr: string,
+//   circuitWasmBuffer: Buffer,
+//   zkeyBuffer: Buffer
+// ): Promise<string> {
+//   const inputs = await generateMerkleCircuitInputJson(
+//     merkleTree,
+//     BigInt(mainAddr),
+//     BigInt(receiverAddr)
+//   );
+
+//   const { proof, publicSignals } = await snarkjs.plonk.fullProve(
+//     inputs,
+//     circuitWasmBuffer,
+//     zkeyBuffer
+//   );
+
+//   const proofProcessed = unstringifyBigInts(proof);
+//   const pubProcessed = unstringifyBigInts(publicSignals);
+//   const allSolCallData: string = await snarkjs.plonk.exportSolidityCallData(
+//     proofProcessed,
+//     pubProcessed
+//   );
+//   const solCallDataProof = allSolCallData.split(',')[0];
+//   return solCallDataProof;
+// }
+ 
+
+function splitPrivateKey(privateKeyHex: string): number[] {
+  if (privateKeyHex.length !== 64) {
+    throw new Error('Invalid private key length. Expected 64 hex characters.');
+  }
+
+  // 将私钥转换为 BigInt 类型的十进制值
+  const privateKeyDecimal = BigInt(privateKeyHex);
+
+  // 计算每部分的大小
+  const chunkSize = privateKeyDecimal / BigInt(4);
+
+  // 分割私钥
+  const privateKeyChunks = [
+    Number(privateKeyDecimal - chunkSize * BigInt(3)),
+    Number(privateKeyDecimal - chunkSize * BigInt(2)),
+    Number(privateKeyDecimal - chunkSize * BigInt(1)),
+    Number(privateKeyDecimal),
+  ];
+  return privateKeyChunks;
+}
+
+interface SigCircuitInput {
+  privkey: number[];
+}
+
+interface MerkleCircuitInput {
+  root: BigInt;
+  commitment: BigInt;
+  pathIndices: number[];
+  pathElements: BigInt[];
+  recipient: BigInt;
+}
+
+async function generateSigCircuitInputJson(privateKeyChunks: number[]): Promise<SigCircuitInput> {
+  const inputObj = {
+    privkey: privateKeyChunks,
+  };
+  return inputObj;
+}
+
+async function generateMerkleCircuitInputJson(
+  mt: MerkleTree,
+  mainAddrBi: BigInt,
+  receiverAddr: BigInt
+): Promise<MerkleCircuitInput> {
+  const commitment = await poseidon1(mainAddrBi);
+  const mp = mt.getMerkleProof(commitment);
+
+  const inputObj = {
+    root: mt.root.val,
+    commitment,
+    pathIndices: mp.indices,
+    pathElements: mp.vals,
+    recipient: receiverAddr,
+  };
+  return inputObj;
+}
+
+// Lifted from ffutils: https://github.com/iden3/ffjavascript/blob/master/src/utils_bigint.js
+function unstringifyBigInts(o: any): any {
+  if (typeof o === 'string' && /^[0-9]+$/.test(o)) {
+    return BigInt(o);
+  }
+  if (typeof o === 'string' && /^0x[0-9a-fA-F]+$/.test(o)) {
+    return BigInt(o);
+  }
+  if (Array.isArray(o)) {
+    return o.map(unstringifyBigInts);
+  }
+  if (typeof o === 'object') {
+    const res: { [key: string]: any } = {};
+    const keys = Object.keys(o);
+    keys.forEach((k) => {
+      res[k] = unstringifyBigInts(o[k]);
+    });
+    return res;
+  }
+  return o;
+}
+
+function toBufferLE(bi: BigInt, width: number): Buffer {
+  const hex = bi.toString(16);
+  const buffer = Buffer.from(hex.padStart(width * 2, '0').slice(0, width * 2), 'hex');
+  buffer.reverse();
+  return buffer;
+}
---
+++ src/sections/home/zklib/zkutil.ts
@@ -0,0 +1,60 @@
+import { providers } from 'ethers';
+import { generateSigProofCallData, generateMerkleProofCallData } from './Library';
+import { MerkleTree } from './MerkleTree';
+
+// for verify the eth_addr ownership
+async function calculateSigProof(privateKey: string, proof: string) {
+  // Connect to wallet, get address
+  const provider = new providers.Web3Provider(window.ethereum as any);
+  await provider.send('eth_requestAccounts', []);
+  const signer = provider.getSigner();
+  const address = await signer.getAddress();
+
+  // Load files and run proof locally
+  // TODO: zkey file path
+  const zkFilePath = '';
+  const wasmBuff = await getFileBuffer(`${zkFilePath}/circuit.wasm`);
+  const zkeyBuff = await getFileBuffer(`${zkFilePath}/circuit_final.zkey`);
+
+  const preTime = new Date().getTime();
+  proof = await generateSigProofCallData(privateKey, wasmBuff, zkeyBuff);
+  const elapsed = new Date().getTime() - preTime;
+  console.log(`Time to compute proof: ${elapsed}ms`);
+}
+
+// for verify the eth_addr hold certain resource
+async function calculateMerkleProof(mainAddr: string, proof: string) {
+  // Connect to wallet, get address
+  const provider = new providers.Web3Provider(window.ethereum as any);
+  await provider.send('eth_requestAccounts', []);
+  const signer = provider.getSigner();
+  const address = await signer.getAddress();
+
+  // Load files and run proof locally
+  // TODO: zkey file path
+  const zkFilePath = '';
+  // TODO:fetch address set
+  // fetch address set done
+  const mtSs = await getFileString(`${zkFilePath}/mt_8192.txt`);
+  const wasmBuff = await getFileBuffer(`${zkFilePath}/circuit.wasm`);
+  const zkeyBuff = await getFileBuffer(`${zkFilePath}/circuit_final.zkey`);
+
+  // Load the Merkle Tree locally
+  const mt = MerkleTree.createFromStorageString(mtSs);
+
+  const preTime = new Date().getTime();
+  const biMainAddr = BigInt(mainAddr);
+  proof = await generateMerkleProofCallData(mt, biMainAddr, address, wasmBuff, zkeyBuff);
+  const elapsed = new Date().getTime() - preTime;
+  console.log(`Time to compute proof: ${elapsed}ms`);
+}
+
+
+async function getFileString(filename: string) {
+  const req = await fetch(filename);
+  return req.text();
+}
+async function getFileBuffer(filename: string) {
+  const req = await fetch(filename);
+  return Buffer.from(await req.arrayBuffer());
+}
